240a17c38d17f00ae7d7fd0287bf255ceb2bd378
Preston Harms <prestonharms@microsoft.com>
2025-02-18 10:08:57 -0800

    Added logger

---
+++ src/logging/start_up_logger.py
@@ -0,0 +1,8 @@
+import functools
+
+def log_config_method_call(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Configuration called: {func.__name__} with args: {args} kwargs: {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
